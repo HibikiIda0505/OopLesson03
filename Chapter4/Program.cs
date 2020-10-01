3948f6f75cfe81130eb08b1a0e57ed6cf4f80b08
IdaHibiki <31062@ojs.ac.jp>
2020-10-01 10:37:45 +0900

    null条件演算子

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,20 +10,26 @@ namespace Chapter4
     class Program
     {
         static void Main(string[] args) {
-            string code = "12345";
+            Console.WriteLine(GetProduct());
 
-            var message = GetMessage(code) ?? DefaultMessage();
-            Console.WriteLine(message);
         }
 
-        //スタブ
-        private static object DefaultMessage() {
-            return "DefaultMessage";
+        private static string GetProduct() {
+            Sale sale = new Sale {
+                ShopName = "pet store",
+                Amount = 100000,
+                Product = "food"
+            };
+            sale = null;
+            return sale?.Product;
         }
+    }
 
-        //スタブ
-        private static object GetMessage(string code){
-            return 123;        
-        }
+    class Sale {
+        //店舗名
+        public string ShopName { get; set; }
+        //売上高
+        public int Amount { get; set; }
+        public string Product { get; set; }
     }
 }
