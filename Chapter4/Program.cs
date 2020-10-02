eddb58d0fca30a10edc8dfcab15bf9c9d337c741
IdaHibiki <31062@ojs.ac.jp>
2020-10-02 14:06:49 +0900

    問題4.1完成

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -9,27 +9,9 @@ namespace Chapter4
 {
     class Program
     {
-        static void Main(string[] args) {
-            Console.WriteLine(GetProduct());
+        static void Main(string[] args)
+        {
 
         }
-
-        private static string GetProduct() {
-            Sale sale = new Sale {
-                ShopName = "pet store",
-                Amount = 100000,
-                Product = "food"
-            };
-            sale = null;
-            return sale?.Product;
-        }
-    }
-
-    class Sale {
-        //店舗名
-        public string ShopName { get; set; }
-        //売上高
-        public int Amount { get; set; }
-        public string Product { get; set; }
     }
 }
