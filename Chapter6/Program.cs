5c23d1165bc638ac8092bed11ccdd807365b0ddf
IdaHibiki <31062@ojs.ac.jp>
2020-10-15 10:02:20 +0900

    P196まで

---
+++ Chapter6/Program.cs
@@ -0,0 +1,27 @@
+﻿using Chapter06;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////すべての書籍から[C#]の文字がいくつあるかをカウントする
+            //int count = 0;
+            //foreach (var book in books.Where(b => b.Title.Contains("C#"))) {
+            //    for (int i = 0; i < book.Title.Length - 1; i++) {
+            //        if (book.Title[i] == 'C') && (book.Title[i + 1] == '#')) {
+            //        count++;
+                    
+            //    }
+            //}
+            //Console.WriteLine($"文字列「C#」の個数は{count}です。");
+
+        }
+    }
+}
