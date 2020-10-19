4a12c66022e4d75e2112695131ff88072d55d2f0
IdaHibiki <31062@ojs.ac.jp>
2020-10-19 13:36:24 +0900

    問題7.2終了

--- Chapter7/Program.cs
+++ Chapter7/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,46 +13,59 @@ namespace Chapter7
     {
         static void Main(string[] args)
         {
-            var text = "Cozy lummox gives smart squid who asks for job pen";
-            Exercise1_1(text);  //問題7.1.1
+            // コンストラクタ呼び出し
+            var abbrs = new Abbreviations();
 
-        }
+            // Addメソッドの呼び出し例
+            abbrs.Add("IOC", "国際オリンピック委員会");
+            abbrs.Add("NPT", "核兵器不拡散条約");
 
-        static void Exercise1_1(string text)
-        {
-            var dict = new SortedDictionary<char, int>();
-            //foreachで一文字ずつ取り出す
-            foreach (var ch in text)
-            {
-                char upc = char.ToUpper(ch);
-                //(半角アルファベットか調べる処理)
-                //大文字へ変換
-                //if ('A' <= char && char <= 'Z') {   //chは文字列型
-                //     keyに存在するか？
-                //     ・存在する
-                //          対応するValueの値をインクリメント
-                //     ・存在しない
-                //          単語をkeyへ登録してValueに1を設定
-                //}
-                if ('A' <= upc && upc <= 'Z')
-                {
-                    if (dict.ContainsKey(upc))          
-                    {
-                        //既に登録済み
-                        dict[upc]++;
-                    }
-                    else
-                    {
-                        //未登録
-                        dict[upc] = 1;
-                    }
-                }
+            //問題7.2.3
+            //Countプロパティを呼び出して数を出力させる
+            Console.WriteLine(abbrs.Count);
+            //Removeプロパティを呼び出して要素を削除する
+            if(abbrs.Remove("NPT")){
+                Console.WriteLine("削除成功");
             }
-            //累計が完了したらテキストの出力例を参考に出力して終了
-            foreach (var item in dict)
+            else
             {
-                Console.WriteLine($"{item.Key}:{item.Value}");
+                Console.WriteLine("削除失敗");
             }
+
+
+            // インデクサの利用例
+            var names = new[] { "WHO", "FIFA", "NPT", };
+            foreach (var name in names)
+            {
+                var fullname = abbrs[name];
+                if (fullname == null)
+                    Console.WriteLine("{0}は見つかりません", name);
+                else
+                    Console.WriteLine("{0}={1}", name, fullname);
+            }
+            Console.WriteLine();
+
+            // ToAbbreviationメソッドの利用例
+            var japanese = "東南アジア諸国連合";
+            var abbreviation = abbrs.ToAbbreviation(japanese);
+            if (abbreviation == null)
+                Console.WriteLine("{0} は見つかりません", japanese);
+            else
+                Console.WriteLine("「{0}」の略語は {1} です", japanese, abbreviation);
+            Console.WriteLine();
+
+            // FindAllメソッドの利用例
+            foreach (var item in abbrs.FindAll("国際"))
+            {
+                Console.WriteLine("{0}={1}", item.Key, item.Value);
+            }
+            Console.WriteLine();
+
+            //7.2.4(クラスにも追加あり)
+            foreach (var item in abbrs.Where(x => x.Key.Length == 3))
+            {
+                Console.WriteLine($"{item.Key} = {item.Value}");
+            } 
         }
     }
 }
